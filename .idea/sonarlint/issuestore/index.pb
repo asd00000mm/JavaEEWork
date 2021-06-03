cb97f7b065903d1e559a0f238cd3744eda4c7b88
asd00000mm <535730906@qq.com>
2021-06-03 19:35:10 +0800

    v1.0

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-W
-'src/main/java/pers/lyz/Application.java,1\c\1c0f763f94e3ac8c76937ff2c0dc865a8c7f6264
---
+++ src/main/resources/lyz.sql
@@ -0,0 +1,42 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : lyz
+ Source Server Type    : MySQL
+ Source Server Version : 50728
+ Source Host           : localhost:3306
+ Source Schema         : javaee
+
+ Target Server Type    : MySQL
+ Target Server Version : 50728
+ File Encoding         : 65001
+
+ Date: 03/06/2021 19:32:08
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods`  (
+  `g_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `g_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `g_price` double(10, 2) NULL DEFAULT NULL,
+  `g_number` int(11) NULL DEFAULT NULL,
+  `g_state` varchar(5) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `g_date` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`g_id`) USING BTREE,
+  INDEX `g_id`(`g_id`, `g_name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+INSERT INTO `goods` VALUES ('001', 'iPhone12', 111.00, 10000, '有货', '2021-06-02 00:34:09');
+INSERT INTO `goods` VALUES ('002', 'ipad2020', 3000.00, 5000, '有货', '2021-06-02 00:34:53');
+INSERT INTO `goods` VALUES ('003', '华为', 1111.00, 222, '有货', '2021-06-03 14:11:22');
+
+SET FOREIGN_KEY_CHECKS = 1;
