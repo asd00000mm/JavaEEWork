4405cc3aa17e025989c4605ea5360545d7947952
asd00000mm <535730906@qq.com>
2021-06-05 23:09:28 +0800

    v1.3 将默认访问页面跳转到登录页面。

--- src/main/java/pers/lyz/controller/FirstController.java
+++ src/main/java/pers/lyz/controller/FirstController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author asd00000mm
+ * 启动项目后跳转到登录页面
  */
 @Controller
 public class FirstController {
     @RequestMapping("/")
-    public String hello(){
+    public String login(){
         return "forward:login.html";
     }
 }
